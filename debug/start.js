441d9000d3657fcdffec4e1cc887d6f3bb846c8a
zhuling822 <ling.zhu@js.design>
2024-05-28 20:35:27 +0800

    fix: del lint & 标记初始化流程

--- debug/start.js
+++ debug/start.js
@@ -4,8 +4,14 @@ const webpack = require('../lib/index.js')
 // 引入上面写的webpack配置对象
 const config = require('./webpack.config')
 // 创建一个complier对象
+// TODO-初始化1：启动
 const complier = webpack(config)
 // 执行compiler.run方法开始编译代码，回调方法用于反馈编译的状态
+
+
+
+
+// 开始编译：执行 compiler 对象的 run 方法
 complier.run((err, stats) => {
   // 如果运行时报错输出报错
   if (err) {
